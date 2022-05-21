5a503689ca484dd94dbf454d635c4807e92285b9
loidinhm31 <loidinh.git@gmail.com>
2022-05-21 23:47:31 +0700

    refactor: add on click listener for recycler view

--- app/src/main/java/com/july/wikipedia/localstore/repositories/FavoritesRepository.kt
+++ app/src/main/java/com/july/wikipedia/localstore/repositories/FavoritesRepository.kt
@@ -1,31 +1,25 @@
 package com.july.wikipedia.localstore.repositories
 
-import com.google.gson.Gson
 import com.july.wikipedia.models.WikiPage
-import com.july.wikipedia.models.WikiThumbnail
-import org.jetbrains.anko.db.insert
-import org.jetbrains.anko.db.delete
-import org.jetbrains.anko.db.rowParser
-import org.jetbrains.anko.db.select
 
 class FavoritesRepository(val databaseHelper: ArticleDatabaseOpenHelper) {
     private val TABLE_NAME: String = "Favorites"
 
     fun addFavorite(page: WikiPage) {
-        databaseHelper.use {
-            insert(TABLE_NAME,
-                    "id" to page.pageid,
-                    "title" to page.title,
-                    "url" to page.fullurl,
-                    "thumbnailJson" to Gson().toJson(page.thumbnail))
-
-        }
+//        databaseHelper.use {
+//            insert(TABLE_NAME,
+//                    "id" to page.pageid,
+//                    "title" to page.title,
+//                    "url" to page.fullurl,
+//                    "thumbnailJson" to Gson().toJson(page.thumbnail))
+//
+//        }
     }
 
     fun removeFavoriteById(pageId: Int) {
-        databaseHelper.use {
-            delete(TABLE_NAME, "id = {pageId}", "pageId" to pageId)
-        }
+//        databaseHelper.use {
+//            delete(TABLE_NAME, "id = {pageId}", "pageId" to pageId)
+//        }
     }
 
     fun isArticleFavorite(pageId: Int) : Boolean {
@@ -40,20 +34,20 @@ class FavoritesRepository(val databaseHelper: ArticleDatabaseOpenHelper) {
     fun getAllFavorites() : ArrayList<WikiPage> {
         var pages = ArrayList<WikiPage>()
 
-        val articleRowParser = rowParser { id: Int, title: String, url: String, thumbnailJson: String ->
-            val page = WikiPage()
-            page.title = title
-            page.pageid = id
-            page.fullurl = url
-            page.thumbnail = Gson().fromJson(thumbnailJson, WikiThumbnail::class.java)
-
-            pages.add(page)
-        }
-
-        // use database to update UI
-        databaseHelper.use {
-            select(TABLE_NAME).parseList(articleRowParser)
-        }
+//        val articleRowParser = rowParser { id: Int, title: String, url: String, thumbnailJson: String ->
+//            val page = WikiPage()
+//            page.title = title
+//            page.pageid = id
+//            page.fullurl = url
+//            page.thumbnail = Gson().fromJson(thumbnailJson, WikiThumbnail::class.java)
+//
+//            pages.add(page)
+//        }
+//
+//        // use database to update UI
+//        databaseHelper.use {
+//            select(TABLE_NAME).parseList(articleRowParser)
+//        }
 
         return pages
     }
--- app/src/main/java/com/july/wikipedia/localstore/repositories/HistoryRepository.kt
+++ app/src/main/java/com/july/wikipedia/localstore/repositories/HistoryRepository.kt
@@ -1,51 +1,45 @@
 package com.july.wikipedia.localstore.repositories
 
-import com.google.gson.Gson
 import com.july.wikipedia.models.WikiPage
-import com.july.wikipedia.models.WikiThumbnail
-import org.jetbrains.anko.db.delete
-import org.jetbrains.anko.db.insert
-import org.jetbrains.anko.db.rowParser
-import org.jetbrains.anko.db.select
 
 class HistoryRepository(val databaseHelper: ArticleDatabaseOpenHelper) {
     private val TABLE_NAME: String = "History"
 
     fun addFavorite(page: WikiPage) {
-        databaseHelper.use {
-            insert(TABLE_NAME,
-                "id" to page.pageid,
-                "title" to page.title,
-                "url" to page.fullurl,
-                "thumbnailJson" to Gson().toJson(page.thumbnail))
-
-        }
+//        databaseHelper.use {
+//            insert(TABLE_NAME,
+//                "id" to page.pageid,
+//                "title" to page.title,
+//                "url" to page.fullurl,
+//                "thumbnailJson" to Gson().toJson(page.thumbnail))
+//
+//        }
     }
 
     fun removePageById(pageId: Int) {
-        databaseHelper.use {
-            delete(TABLE_NAME, "id = {pageId}", "pageId" to pageId)
-        }
+//        databaseHelper.use {
+//            delete(TABLE_NAME, "id = {pageId}", "pageId" to pageId)
+//        }
     }
 
 
     fun getAllHistory() : ArrayList<WikiPage> {
         var pages = ArrayList<WikiPage>()
 
-        val articleRowParser = rowParser { id: Int, title: String, url: String, thumbnailJson: String ->
-            val page = WikiPage()
-            page.title = title
-            page.pageid = id
-            page.fullurl = url
-            page.thumbnail = Gson().fromJson(thumbnailJson, WikiThumbnail::class.java)
-
-            pages.add(page)
-        }
-
-        // use database to update UI
-        databaseHelper.use {
-            select(TABLE_NAME).parseList(articleRowParser)
-        }
+//        val articleRowParser = rowParser { id: Int, title: String, url: String, thumbnailJson: String ->
+//            val page = WikiPage()
+//            page.title = title
+//            page.pageid = id
+//            page.fullurl = url
+//            page.thumbnail = Gson().fromJson(thumbnailJson, WikiThumbnail::class.java)
+//
+//            pages.add(page)
+//        }
+//
+//        // use database to update UI
+//        databaseHelper.use {
+//            select(TABLE_NAME).parseList(articleRowParser)
+//        }
 
         return pages
     }
